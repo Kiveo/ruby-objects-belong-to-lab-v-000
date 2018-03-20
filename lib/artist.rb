6213a057c663be965c39a0f8f4454083ba9149fb
Maverick Tesini <mavericktesini@gmail.com>
2018-03-20 20:49:26 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,5 @@
 class Artist
-  attr_accessor :name
+  # attr_accessor :name
 
   def initialize(name)
     @name = name
@@ -7,6 +7,9 @@ class Artist
 
   def name=(name)
     @name = name
+  end
+  def name
+    @name
   end
 
 end
